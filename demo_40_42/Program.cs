c5ad6ee742133e9eda3fb0655785e406ba72d2a0
VolodinAlekse <volodin.8989@mail.ru>
2023-02-18 22:50:15 +0300

    recursia

--- demo_40_42/Program.cs
+++ demo_40_42/Program.cs
@@ -22,17 +22,28 @@
 
 
 
-System.Console.WriteLine("Ведите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-string st = "";
+// System.Console.WriteLine("Ведите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string st = "";
 
-while (n != 0)
+// while (n != 0)
+// {
+//     st += n % 2;
+//     n = n / 2;
+// }
+// for (int i = st.Length - 1; i > 0; i--)
+// {
+//     System.Console.Write(st[i]);
+// }
+// System.Console.WriteLine();
+
+void GetBinatyView(int number)
 {
-    st += n % 2;
-    n = n / 2;
+    if (number <= 0) return;
+    GetBinatyView(number / 2);
+    System.Console.Write(number % 2);
 }
-for (int i = st.Length - 1; i > 0; i--)
-{
-    System.Console.Write(st[i]);
-}
-System.Console.WriteLine();
+
+System.Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+GetBinatyView(N);

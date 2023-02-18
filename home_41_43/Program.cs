33f622c9dc52ea851ccb1cb72ba7336f3e99f599
VolodinAlekse <volodin.8989@mail.ru>
2023-02-19 00:07:39 +0300

    home_43

--- home_41_43/Program.cs
+++ home_41_43/Program.cs
@@ -3,72 +3,104 @@
 //1, -7, 567, 89, 223-> 3
 
 
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Введите числа через запятую: ");
+//         int[] numbers = StringNumbers(Console.ReadLine()!);
+//         PrintArray(numbers);
+
+//         int sum = 0;
+//         for (int i = 0; i < numbers.Length; i++)
+//         {
+//             if (numbers[i] > 0)
+//             {
+//                 sum++;
+//             }
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+//         int[] StringNumbers(string input)
+//         {
+//             int count = 1;
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 if (input[i] == ',')
+//                 {
+//                     count++;
+//                 }
+//             }
+
+//             int[] numbers = new int[count];
+//             int index = 0;
+
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 string temp = "";
+
+//                 while (input[i] != ',')
+//                 {
+//                     if (i != input.Length - 1)
+//                     {
+//                         temp += input[i].ToString();
+//                         i++;
+//                     }
+//                     else
+//                     {
+//                         temp += input[i].ToString();
+//                         break;
+//                     }
+//                 }
+//                 numbers[index] = Convert.ToInt32(temp);
+//                 index++;
+//             }
+//             return numbers;
+//         }
+
+
+//         void PrintArray(int[] array)
+//         {
+//             Console.Write("[ ");
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 Console.Write(array[i] + " ");
+//             }
+//             Console.Write("]");
+//         }
+//     }
+// }
+
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Введите числа через запятую: ");
-        int[] numbers = StringNumbers(Console.ReadLine()!);
-        PrintArray(numbers);
-
-        int sum = 0;
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            if (numbers[i] > 0)
-            {
-                sum++;
-            }
-        }
-        Console.WriteLine();
-        Console.WriteLine($"количество значений больше 0 = {sum}");
+        Console.Write("Введите b1: ");
+        var b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите k1: ");
+        var k1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите b2: ");
+        var b2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите k2: ");
+        var k2 = Convert.ToDouble(Console.ReadLine());
 
 
-        int[] StringNumbers(string input)
-        {
-            int count = 1;
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i] == ',')
-                {
-                    count++;
-                }
-            }
+        var x = -(b1 - b2) / (k1 - k2);
+        var y = k1 * x + b1;
 
-            int[] numbers = new int[count];
-            int index = 0;
+        x = Math.Round(x, 3);
+        y = Math.Round(y, 3);
 
-            for (int i = 0; i < input.Length; i++)
-            {
-                string temp = "";
-
-                while (input[i] != ',')
-                {
-                    if (i != input.Length - 1)
-                    {
-                        temp += input[i].ToString();
-                        i++;
-                    }
-                    else
-                    {
-                        temp += input[i].ToString();
-                        break;
-                    }
-                }
-                numbers[index] = Convert.ToInt32(temp);
-                index++;
-            }
-            return numbers;
-        }
-
-
-        void PrintArray(int[] array)
-        {
-            Console.Write("[ ");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
-            Console.Write("]");
-        }
+        Console.WriteLine($"Пересечение в точке: ({x};{y})");
     }
 }

78d978f60ca9e67941d7b1d1f99042c934bec2e0
VolodinAlekse <volodin.8989@mail.ru>
2023-02-18 22:42:16 +0300

    demo_42

---
+++ demo_40_42/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 40: Напишите программу, 
+// которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+// metka:
+// System.Console.WriteLine("Введите числа: ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// if (array.Length != 3)
+// {
+//     System.Console.WriteLine("Массив введен некоректно ");
+//     goto metka;
+// }
+
+// if ((array[0] + array[1]) > array[2] && (array[1] + array[2]) > array[0] && (array[0] + array[2]))
+// {
+//     System.Console.WriteLine("Такой треугольник существует ");
+// }
+// else
+// {
+//     System.Console.WriteLine("Такой треугольник не существует ");
+// }
+
+
+
+System.Console.WriteLine("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string st = "";
+
+while (n != 0)
+{
+    st += n % 2;
+    n = n / 2;
+}
+for (int i = st.Length - 1; i > 0; i--)
+{
+    System.Console.Write(st[i]);
+}
+System.Console.WriteLine();
